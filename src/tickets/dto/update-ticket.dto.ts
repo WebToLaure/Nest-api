92e6623bd1ec9343398ace98d5ab5690cbd12e7f
WebToLaure <lmarmus@hotmail.fr>
2023-02-02 22:46:22 +0100

    fix: delete ticket

--- src/tickets/dto/update-ticket.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types'; // nous permet d'éviter les codes redondants avec importation notamment de PartialType
-import { CreateTicketDto } from './create-ticket.dto';
-
-export class UpdateTicketDto extends PartialType (CreateTicketDto) {}// nous retourne les types que nous avons choisi dans create-ticket
-
-
